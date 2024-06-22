a8225db152492462c43ec64616bf99199af0d3e5
Artur Prepelytsia <preart81@gmail.com>
2024-06-22 16:09:01 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+const onFormSubmit = (event) => {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    alert("All form fields must be filled in");
+    return;
+  } else {
+    const formData = {
+      email: email.value.trim(),
+      password: password.value.trim(),
+    };
+
+    console.log(formData);
+    event.currentTarget.reset();
+  }
+};
+
+form.addEventListener("submit", onFormSubmit);
